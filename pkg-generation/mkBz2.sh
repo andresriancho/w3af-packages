5ef939598f708c82c3cd14be4959a986d3d58c19
adam_baldwin <adam_baldwin@16c29cf1-982c-0410-8ff8-8bb040e68b5b>
2008-05-04 19:17:19 +0000

    Updated for new path location in SVN repo and does not squash local trunk

--- pkg-generation/mkBz2.sh
+++ pkg-generation/mkBz2.sh
@@ -1,25 +1,27 @@
 #!/bin/bash
 
+# copy trunk so we don't destroy our local copy
+cp -Rp ../../trunk ../../w3af
+
 # remove the compiled python modules
-find ../w3af/ -name *.pyc | xargs rm
+find -L ../../w3af -name *.pyc | xargs rm
 
 # remove the backup files
-find ../w3af/ -name '*~' | xargs rm
+find -L ../../w3af -name '*~' | xargs rm
 
 # remove the svn stuff
-find ../w3af/ -name .svn | xargs rm -rf
+find -L ../../w3af -name .svn | xargs rm -rf
 
 # remove some paths and files that are created during the run
-rm ../w3af/.urllib2cache/ -rf
-rm ../w3af/.tmp/ -rf
-rm ../w3af/output-*.txt -rf
-rm ../w3af/sessions/* -rf
-rm ../w3af/w3af.e3* -rf
-rm ../w3af/extras/ -rf
+rm ../../w3af/.urllib2cache/ -rf
+rm ../../w3af/.tmp/ -rf
+rm ../../w3af/output-*.txt -rf
+rm ../../w3af/sessions/* -rf
+rm ../../w3af/w3af.e3* -rf
+rm ../../w3af/extras/ -rf
 
 # Create the tar archive
-cd ..
-tar -cpvf w3af-`date +%d%b%Y`.tar w3af/
+tar -chpvf w3af-`date +%d%b%Y`.tar ../../w3af
 
 echo "Creating bz2 file."
 
@@ -28,4 +30,7 @@ bzip2 w3af-`date +%d%b%Y`.tar
 echo "File created!"
 
 du -sh  w3af-`date +%d%b%Y`.tar.bz2
+
+# Cleanup
+rm -rf ../../w3af
 
