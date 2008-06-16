0fbbef0df6c9b5c2cd62ffc20342fdfde556a156
lbello <lbello@16c29cf1-982c-0410-8ff8-8bb040e68b5b>
2008-06-16 18:19:42 +0000

    debian package initial release

--- debian/mk_debian_directory.sh
+++ debian/mk_debian_directory.sh
@@ -1,53 +1,89 @@
 #!/bin/bash
+TRUNK='../../w3af'
+
+UPSTREAM_RELEASE="0.0.0"
+REVISION=$(svn info $TRUNK | grep Revision | cut -f 2 -d ' ')
+VERSION="${UPSTREAM_RELEASE}svn${REVISION}"
+BASE_DIR="w3af-${VERSION}"
 
 # Just in case...
-rm -rf w3af
+rm -rf ${BASE_DIR}
+rm w3af_${VERSION}.orig.tar.gz
 
 # copy trunk so we don't destroy our local copy
-cp -Rp ../../trunk w3af
+cp -Rp $TRUNK ${BASE_DIR}
+
+# copy the manpage
+cp -Rp ../manpage/w3af.1 ${BASE_DIR}
 
 # remove the compiled python modules
-find -L w3af -name *.pyc | xargs rm
+find -L ${BASE_DIR} -name *.pyc | xargs rm
 
 # remove the backup files
-find -L w3af -name '*~' | xargs rm
+find -L ${BASE_DIR} -name '*~' | xargs rm
 
 # remove the svn stuff
-find -L w3af -name .svn | xargs rm -rf
+find -L ${BASE_DIR} -name .svn | xargs rm -rf
+
+# remove plugins started with _
+find w3af-0.0.0svn1305/plugins/ -name "_*.py" | grep -v init | xargs rm
 
 # remove some paths and files that are created during the run
-rm w3af/.urllib2cache/ -rf
-rm w3af/.tmp/ -rf
-rm w3af/output-*.txt -rf
-rm w3af/sessions/* -rf
-rm w3af/w3af.e3* -rf
+rm ${BASE_DIR}/.urllib2cache/ -rf
+rm ${BASE_DIR}/.tmp/ -rf
+rm ${BASE_DIR}/output-*.txt -rf
+rm ${BASE_DIR}/sessions/* -rf
+rm ${BASE_DIR}/w3af.e3* -rf
 
 # This is not ready for distribution yet
-rm w3af/mozilla-extension/ -rf
+rm ${BASE_DIR}/mozilla-extension/ -rf
+
+# sanitize oHalberd
+mv ${BASE_DIR}/plugins/discovery/oHalberd/Halberd ${BASE_DIR}/plugins/discovery/
+rm -rf ${BASE_DIR}/plugins/discovery/oHalberd/
+mv ${BASE_DIR}/plugins/discovery/Halberd ${BASE_DIR}/plugins/discovery/oHalberd
 
 # Remove the debian offending stuff
-rm -rf w3af/extlib/pygoogle/
-rm -rf w3af/extlib/pywordnet/
-rm -rf w3af/plugins/discovery/wordnet.py
+rm -rf ${BASE_DIR}/extlib/pygoogle/
+rm -rf ${BASE_DIR}/extlib/pywordnet/
+rm -rf ${BASE_DIR}/plugins/discovery/wordnet.py
+rm -rf ${BASE_DIR}/tools/
 
 # And remove the things that are already inside debian as packages
 # python-scapy
-rm -rf w3af/extlib/scapy/
-# Created by Luciano Bello for w3af
+rm -rf ${BASE_DIR}/extlib/scapy/
 # python-pypdf
-rm -rf w3af/extlib/pyPdf/
+rm -rf ${BASE_DIR}/extlib/pyPdf/
 # python-beautifulsoup
-rm -rf w3af/extlib/BeautifulSoup.py
+rm -rf ${BASE_DIR}/extlib/BeautifulSoup.py
 # python-json
-rm -rf w3af/extlib/jsonpy/
+rm -rf ${BASE_DIR}/extlib/jsonpy/
 # python-fpconst
-rm -rf w3af/extlib/fpconst-0.7.2/
+rm -rf ${BASE_DIR}/extlib/fpconst*/
 # python-soappy
-rm -rf w3af/extlib/SOAPpy/
+rm -rf ${BASE_DIR}/extlib/SOAPpy/
 # python-buzhug
-echo 'TODO: python-buzhug'
-rm -rf w3af/extlib/buzhug/
+rm -rf ${BASE_DIR}/extlib/buzhug/
 
-# End!
-echo 'The w3af directory inside "." contains all you need to create the .deb package.'
+#fixing permitions
+find ${BASE_DIR} -type d -exec chmod 755 {} \;
+find ${BASE_DIR} -type f -exec chmod 644 {} \;
+chmod +x ${BASE_DIR}/w3af
+
+echo "creating w3af_${VERSION}.orig.tar.gz"
+tar zcf w3af_${VERSION}.orig.tar.gz ${BASE_DIR}
+
+echo '~/debian'
+cp -r trunk/debian ${BASE_DIR}/
+
+echo 'copy diffs'
+cp -Rp dependencyCheck.py ${BASE_DIR}/core/controllers/misc/dependencyCheck.py
+
+echo 'setting changelog'
+sed -i "s/\[w3af_VERSION\]/$VERSION/" ${BASE_DIR}/debian/changelog
+
+echo 'setting desktop'
+sed -i "s/\[w3af_VERSION\]/$VERSION/" ${BASE_DIR}/debian/desktop
+
+echo "The w3af directory inside ${BASE_DIR} contains all you need to create the .deb package."
 
---
+++ debian/trunk/debian/w3af.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+/usr/share/w3af/w3af
