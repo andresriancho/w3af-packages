947c30a1d5e9c760937573b408b4487d841201a4
ulises2k <ulises2k@16c29cf1-982c-0410-8ff8-8bb040e68b5b>
2010-01-12 08:06:46 +0000

    New version of GTK2-Runtime. New/Update themes. Add gtk2_prefs for choice Themes

---
+++ windows_installer/gtk2-runtime/gtk2-runtime/gtk2r-env.bat
@@ -0,0 +1,9 @@
+@set GTK2R_PREFIX=C:\Archivos de programa\GTK2-Runtime\bin
+
+@echo Setting environment variables for GTK2-Runtime.
+@echo.
+
+@echo set PATH=%GTK2R_PREFIX%;%%PATH%%
+@set PATH=%GTK2R_PREFIX%;%PATH%
+
+@echo.
