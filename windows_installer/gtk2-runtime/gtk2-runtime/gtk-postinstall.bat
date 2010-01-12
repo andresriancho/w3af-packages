424233e3a015da0b26dbd201f827328d75c3d30c
ulises2k <ulises2k@16c29cf1-982c-0410-8ff8-8bb040e68b5b>
2010-01-12 12:46:24 +0000

    Deleted

--- windows_installer/gtk2-runtime/gtk2-runtime/gtk-postinstall.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"C:\Archivos de programa\GTK2-Runtime\bin\pango-querymodules.exe" > "C:\Archivos de programa\GTK2-Runtime\etc\pango\pango.modules"
-rem "C:\Archivos de programa\GTK2-Runtime\bin\gdk-pixbuf-query-loaders.exe" > "C:\Archivos de programa\GTK2-Runtime\etc\gtk-2.0\gdk-pixbuf.loaders"
-"C:\Archivos de programa\GTK2-Runtime\bin\gtk-query-immodules-2.0.exe" > "C:\Archivos de programa\GTK2-Runtime\etc\gtk-2.0\gtk.immodules"
-rem "C:\Archivos de programa\GTK2-Runtime\bin\gtk-update-icon-cache.exe"
--- windows_installer/gtk2-runtime/gtk2-runtime/gtk2r-env.bat
+++
@@ -1,9 +0,0 @@
-@set GTK2R_PREFIX=C:\Archivos de programa\GTK2-Runtime\bin
-
-@echo Setting environment variables for GTK2-Runtime.
-@echo.
-
-@echo set PATH=%GTK2R_PREFIX%;%%PATH%%
-@set PATH=%GTK2R_PREFIX%;%PATH%
-
-@echo.
