4b5c87dfced0494bb492f02bb6a007c7860c093e
lbello <lbello@16c29cf1-982c-0410-8ff8-8bb040e68b5b>
2010-05-11 02:13:54 +0000

    debian:   * dir_bruter/common_dirs_small.db license added to the copyright file.   [ Bernd Zeimetz ]   * Replace python-json by python-simplejson. (Closes: #571110, #580645)   * Build for all Python versions and use the default Python version.

--- debian/trunk/debian/w3af.sh
+++ debian/trunk/debian/w3af.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 if [ ! -z "$DISPLAY" -a -r /usr/share/w3af/w3af_gui ] ; then 
-   /usr/bin/python2.5 /usr/share/w3af/w3af_gui $@
+   /usr/bin/python /usr/share/w3af/w3af_gui $@
 else
-   /usr/bin/python2.5 /usr/share/w3af/w3af_console $@
+   /usr/bin/python /usr/share/w3af/w3af_console $@
 fi
