1a3aebcca0ffd55f633d6e7c0ccdb7140b663cad
andresriancho <andresriancho@16c29cf1-982c-0410-8ff8-8bb040e68b5b>
2010-12-09 14:09:07 +0000

    Updating script to create new package.

--- pkg-generation/mkBz2.sh
+++ pkg-generation/mkBz2.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
+if [ $# != 1 ]; then
+    echo 'Usage: mkBz2.sh <tag-name>'
+    exit
+fi
+
 set -x
 
+# download the newly created tag
+cd ../../tags/
+svn up
+
+# now we copy the tag to a temp directory
+cd ../extras/pkg-generation/
+
 # copy trunk so we don't destroy our local copy
-cp -Rp ../../branches/1.0/ ../../w3af
+cp -Rp ../../tags/$1/ ../../w3af
 
 # remove the compiled python modules
 find -L ../../w3af -name *.pyc | xargs rm
@@ -19,33 +31,27 @@ rm ../../w3af/.urllib2cache/ -rf
 rm ../../w3af/.tmp/ -rf
 rm ../../w3af/output*.* -rf
 rm ../../w3af/gprof2dot.py -rf
-rm ../../w3af/w3af.e3* -rf
-rm ../../w3af/w3af.e4* -rf
 rm ../../w3af/w3af_crash.txt -rf
 
-# remove the things that are under development
-rm ../../mozilla-extension/ -rf
-
-# Remove plugins under development
-rm ../../w3af/plugins/discovery/_web20Spider.py -rf
-rm ../../w3af/plugins/discovery/_mailer.py -rf
-
+# clean previous builds that I made today
+rm w3af-$1.tar
+rm w3af-$1.tar.bz2
 
 # Create the tar archive
-tar -chpvf w3af-`date +%d%b%Y`.tar ../../w3af
+tar -chpvf w3af-$1.tar ../../w3af
 
 echo "Creating bz2 file."
 
-bzip2 w3af-`date +%d%b%Y`.tar
+bzip2 w3af-$1.tar
 
 echo "File created!"
 
-du -sh  w3af-`date +%d%b%Y`.tar.bz2
+du -sh  w3af-$1.tar.bz2
 
 # Cleanup
 rm -rf ../../w3af
 
 set +x
 
-echo "You may upload this file to the FRS here:  https://frs.sourceforge.net/webupload"
-echo "And also using SFTP like this:  http://apps.sourceforge.net/trac/sourceforge/wiki/SFTP"
+echo "You may upload the new package using SFTP following these instructions:"
+echo "http://apps.sourceforge.net/trac/sourceforge/wiki/SFTP"
