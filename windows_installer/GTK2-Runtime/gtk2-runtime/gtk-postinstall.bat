fbce17acc49e28a6714e7fee4b8d52cb073df378
javierandalia <javierandalia@16c29cf1-982c-0410-8ff8-8bb040e68b5b>
2011-02-24 15:30:47 +0000

    Newer versions of gtk, python and graphviz - Added new NSIS plugin

---
+++ windows_installer/GTK2-Runtime/gtk2-runtime/gtk-postinstall.bat
@@ -0,0 +1,5 @@
+@echo off
+"C:\Documents and Settings\Administrador\Escritorio\GTK2-Runtime\bin\pango-querymodules.exe" > "C:\Documents and Settings\Administrador\Escritorio\GTK2-Runtime\etc\pango\pango.modules"
+rem "C:\Documents and Settings\Administrador\Escritorio\GTK2-Runtime\bin\gdk-pixbuf-query-loaders.exe" > "C:\Documents and Settings\Administrador\Escritorio\GTK2-Runtime\etc\gtk-2.0\gdk-pixbuf.loaders"
+"C:\Documents and Settings\Administrador\Escritorio\GTK2-Runtime\bin\gtk-query-immodules-2.0.exe" > "C:\Documents and Settings\Administrador\Escritorio\GTK2-Runtime\etc\gtk-2.0\gtk.immodules"
+rem "C:\Documents and Settings\Administrador\Escritorio\GTK2-Runtime\bin\gtk-update-icon-cache.exe"
---
+++ windows_installer/GTK2-Runtime/gtk2-runtime/gtk2r-env.bat
@@ -0,0 +1,9 @@
+@set GTK2R_PREFIX=C:\Documents and Settings\Administrador\Escritorio\GTK2-Runtime\bin
+
+@echo Setting environment variables for GTK2-Runtime.
+@echo.
+
+@echo set PATH=%GTK2R_PREFIX%;%%PATH%%
+@set PATH=%GTK2R_PREFIX%;%PATH%
+
+@echo.
