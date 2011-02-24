cf62de4abfc6f902c38d8803a00fc684d55420bb
javierandalia <javierandalia@16c29cf1-982c-0410-8ff8-8bb040e68b5b>
2011-02-24 18:49:59 +0000

    Script renamed

--- windows_installer/dev-portable/copy_graphiz.bat
+++ windows_installer/dev-portable/copy_graphiz.bat
@@ -1,7 +1,7 @@
 set Graphviz=c:\"Archivos de programa"
-set w3af_svn=..\Graphviz2.20
+set w3af_svn=..\Graphviz2.24
 
-xcopy /E /H /R %Graphviz%\Graphviz2.20\*.* %w3af_svn%\
+xcopy /E /H /R %Graphviz%\Graphviz2.24\*.* %w3af_svn%\
 del /F /Q %w3af_svn%\Install.ini
 del /F /Q %w3af_svn%\InstallLog.Txt
 del /F /Q %w3af_svn%\Uninstall.exe
